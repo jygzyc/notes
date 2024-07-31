061ab2f33168499197e745997fe3358680728e0e
ecool <jyg.zyc@outlook.com>
2024-08-01 02:22:57 +0800

    20240801 nav bug fix

--- scripts/discussionFileConverter.py
+++ scripts/discussionFileConverter.py
@@ -215,7 +215,7 @@ def _md_meta_generator(discussion: dict, md_name, md_path):
                     f'  updated: {discussion["updatedAt"][0:10]}\n'
                     f'created: {discussion["createdAt"][0:10]}\n'
                     f'updated: {discussion["updatedAt"][0:10]}\n'
-                    f'authors: [Ecool]\n'
+                    f'authors: [ecool]\n'
                     f'categories: {[label["name"] for label in discussion["labels"]["nodes"]] if discussion["labels"]["nodes"] else []}\n'
                     f'comments: {_is_comment_open(discussion)}\n'
                     f'---\n\n')
