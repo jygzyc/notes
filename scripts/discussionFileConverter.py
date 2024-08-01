06ca5695b65cd22ecfb68ee56d030892f32fe5ce
ecool <jyg.zyc@outlook.com>
2024-08-02 03:03:39 +0800

    20240802 scripts update

--- scripts/discussionFileConverter.py
+++ scripts/discussionFileConverter.py
@@ -216,7 +216,8 @@ def _md_meta_generator(discussion: dict, md_name, md_path):
                     f'created: {discussion["createdAt"][0:10]}\n'
                     f'updated: {discussion["updatedAt"][0:10]}\n'
                     f'authors: [ecool]\n'
-                    f'categories: {[label["name"] for label in discussion["labels"]["nodes"]] if discussion["labels"]["nodes"] else []}\n'
+                    f'categories: {"\n".join("  - " + label["name"] for label in 
+                                             discussion.get("labels", {}).get("nodes", []) if "name" in label)}\n'
                     f'comments: {_is_comment_open(discussion)}\n'
                     f'---\n\n')
     else:
