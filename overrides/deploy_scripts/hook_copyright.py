5031ee78f3c43561d6ef21472d241d678edec2e9
ecool <jyg.zyc@outlook.com>
2025-03-18 22:56:17 +0800

    20250318 update

--- overrides/deploy_scripts/hook_copyright.py
+++ overrides/deploy_scripts/hook_copyright.py
@@ -3,4 +3,4 @@
 from datetime import datetime
 
 def on_config(config, **kwargs):
-    config.copyright = f"版权所有 © 2022-{datetime.now().year} Yves的知识花园"
+    config.copyright = f"版权所有 © 2022-{datetime.now().year} Yves的杂货铺"
